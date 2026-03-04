cd510f20f432edfda4a946850851fd099e3f400b
Tony <tony@local>
2026-03-04 15:58:16 -0500

    chore: fix formatting

--- src/agents/tools/sessions-history-tool.ts
+++ src/agents/tools/sessions-history-tool.ts
@@ -1,11 +1,11 @@
 import { Type } from "@sinclair/typebox";
 import { type OpenClawConfig, loadConfig } from "../../config/config.js";
 import { callGateway } from "../../gateway/call.js";
-import { resolveAgentIdFromSessionKey } from "../../routing/session-key.js";
 import { capArrayByJsonBytes } from "../../gateway/session-utils.fs.js";
 import { jsonUtf8Bytes } from "../../infra/json-utf8-bytes.js";
 import { redactSensitiveText } from "../../logging/redact.js";
 import { readStringValue } from "../../shared/string-coerce.js";
+import { resolveAgentIdFromSessionKey } from "../../routing/session-key.js";
 import { truncateUtf16Safe } from "../../utils.js";
 import {
   describeSessionsHistoryTool,
