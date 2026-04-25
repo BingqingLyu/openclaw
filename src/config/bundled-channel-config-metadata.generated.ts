f7d86ac9cfffead98e8834b7b0a5b475698a0f23
Hana Chang <hana@hanamizuki.tw>
2026-04-26 00:47:24 +0800

    chore(slack): regenerate baselines for ignoreOtherMentions after rebase

--- src/config/bundled-channel-config-metadata.generated.ts
+++ src/config/bundled-channel-config-metadata.generated.ts
@@ -11282,6 +11282,9 @@ export const GENERATED_BUNDLED_CHANNEL_CONFIG_METADATA = [
               requireMention: {
                 type: "boolean",
               },
+              ignoreOtherMentions: {
+                type: "boolean",
+              },
               tools: {
                 type: "object",
                 properties: {
@@ -12192,6 +12195,9 @@ export const GENERATED_BUNDLED_CHANNEL_CONFIG_METADATA = [
                       type: "boolean",
                     },
                     requireMention: {
+                      type: "boolean",
+                    },
+                    ignoreOtherMentions: {
                       type: "boolean",
                     },
                     tools: {
